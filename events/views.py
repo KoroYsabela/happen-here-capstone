d7741398601219d7950f74481c52f74f60bf92c0
KoroYsabela <ysabelabathan2604@gmail.com>
2025-10-30 13:19:01 +0000

    sort: events by closest date

--- events/views.py
+++ events/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
 from django.views import generic
 from .models import Event
-#from django.http import HttpResponse
+
 
 # Create your views here.
 
-#def home_page_view(request):
-#    return HttpResponse("Hello, world!")
 
 class EventList(generic.ListView):
-    #model = Event
-    queryset = Event.objects.all()
+    # Use model + get_queryset to avoid evaluating querysets at import time
+    model = Event
     template_name = "events/index.html"
+    # make the template context variable explicit
+    # default would be event_list
+    context_object_name = 'event_list'
+
+    def get_queryset(self):
+        """Return published events ordered by date."""
+        return Event.objects.filter(status=1).order_by('date')

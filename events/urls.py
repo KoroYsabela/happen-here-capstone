883adf244293ce6df2198a7a7acd6c8b88f70cdf
KoroYsabela <ysabelabathan2604@gmail.com>
2025-10-31 00:27:52 +0000

    add: event booking functionality

--- events/urls.py
+++ events/urls.py
@@ -7,4 +7,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
     path('<slug:slug>/', views.event_detail, name='event_detail'),
+    path('<slug:slug>/book/', views.book_event, name='book_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponseRedirect
-from .models import Event
+from .models import Event, Booking
 
 
 # Create your views here.
@@ -35,3 +37,32 @@ def event_detail(request, slug):
         "events/event_detail.html",
         {"event": event},
     )
+
+@login_required
+def book_event(request, slug):
+    """
+    Make a booking for an event
+    """
+    event = get_object_or_404(Event, slug=slug)
+
+    if request.method == "POST":
+        # Check if there is still space incase of overbooking)
+        if event.capacity <= 0:
+            messages.error(request, "Sorry, this event is fully booked.")
+            return redirect('event_detail', slug=event.slug)
+        
+        # Check if user has already booked this event
+        if Booking.objects.filter(event=event, user=request.user).exists():
+            messages.info(request, "You already booked this event.")
+            return redirect('event_detail', slug=event.slug)
+
+        # Create booking
+        Booking.objects.create(user=request.user, event=event)
+
+        # Decrease capacity by 1
+        event.capacity -= 1
+        event.save()
+
+    messages.success(request, "Your booking was successful!")
+    return HttpResponseRedirect(reverse('event_detail', args=[slug]))
+

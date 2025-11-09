508f093074b8580a7d0f86f89bb53e6b96fd8b49
KoroYsabela <ysabelabathan2604@gmail.com>
2025-11-09 17:48:03 +0000

    add: some styling to django allauth

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_USER_MODEL_EMAIL_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = False
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
